a003aee35f6b15e07b8d854af5588bfff24aa4f3
migueltravado <miaje.travado@gmail.com>
2022-12-01 13:54:47 +0100

    Preprocessing initiate

--- preprocessing_treatment.py
+++ preprocessing_treatment.py
@@ -5,15 +5,15 @@ from scipy import stats
 from sklearn.covariance import EllipticEnvelope
 
 
-headers = [*pd.read_csv('csv/csv_result-sds_PICA_H1.csv', nrows=1)]
 df = pd.read_csv("csv/csv_result-sds_PICA_H1.csv", 
                  decimal=".", delimiter=",", na_values="?", on_bad_lines='skip')
 
 
 # ** TRATAMIENTO VALORES NULOS. SUSTITUCIÓN IN-PLACE POR MEDIA ARITMÉTICA
-df_aux = df.describe()
+n_instances = df.shape[0]
+headers = [*pd.read_csv('csv/csv_result-sds_PICA_H1.csv', nrows=1)]
 for col in headers:
-    if df[col].dtype == 'float64' and df_aux[col]['count'] < 7825:
+    if df[col].dtype == 'float64' and df[col].count() < n_instances:
         mean = df[col].mean()
         df[col].fillna(mean, inplace=True)
     else:
@@ -22,6 +22,8 @@ for col in headers:
 
 df.drop('domain', inplace=True, axis=1)
 print(df.dtypes)
+
+df.dropna()
 
 
 # ** ERROR. INPUT CONTAINS NAN, INFINITY OR A VALUE TOO LARGE FOR DTYPE('float64') 
