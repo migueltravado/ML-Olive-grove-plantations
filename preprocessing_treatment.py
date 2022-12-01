e2a5130ab4d9d3a10012a848e0067492cfd107e8
migueltravado <miaje.travado@gmail.com>
2022-12-01 14:11:50 +0100

    Preprocessing initiate - Restart

--- preprocessing_treatment.py
+++ preprocessing_treatment.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+from sklearn import preprocessing
 from sklearn.covariance import EllipticEnvelope
 
-
+# ** LECTURA DEL ARCHIVO ORIGINAL CSV
 df = pd.read_csv("csv/csv_result-sds_PICA_H1.csv", 
                  decimal=".", delimiter=",", na_values="?", on_bad_lines='skip')
 
@@ -13,17 +14,30 @@ df = pd.read_csv("csv/csv_result-sds_PICA_H1.csv",
 n_instances = df.shape[0]
 headers = [*pd.read_csv('csv/csv_result-sds_PICA_H1.csv', nrows=1)]
 for col in headers:
-    if df[col].dtype == 'float64' and df[col].count() < n_instances:
-        mean = df[col].mean()
-        df[col].fillna(mean, inplace=True)
+    if df[col].dtype == 'float64':
+        if df[col].count() < n_instances:
+            mean = df[col].mean()
+            df[col].fillna(mean, inplace=True)
     else:
         df.drop(col, inplace=True, axis=1)
         headers.remove(col)
 
 df.drop('domain', inplace=True, axis=1)
+headers.remove('domain')
 print(df.dtypes)
 
+# ** ELIMINACIÓN DE INSTANCIAS CON VALORES NULOS. MOTIVOS DE SEGURIDAD
 df.dropna()
+
+# ** NORMALIZACIÓN DE VALORES
+normalizer = preprocessing.MinMaxScaler()
+df_norm = normalizer.fit_transform(df)
+
+# ** CONVERSIÓN DE NP.ARRAY A PD.DATAFRAME
+df_norm = pd.DataFrame(df_norm, columns=headers)
+
+# ** GUARDADO DE CSV CON LOS DATOS NORMALIZADOS Y VALORES NULOS REEMPLAZADOS
+df_norm.to_csv('csv_processed/csv_result-sds_PICA_H1_processed.csv', sep=',')
 
 
 # ** ERROR. INPUT CONTAINS NAN, INFINITY OR A VALUE TOO LARGE FOR DTYPE('float64') 
