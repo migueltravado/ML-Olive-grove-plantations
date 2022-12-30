7c47402f70020ab93cd3adb34c464b3b4ceb053c
migueltravado <miaje.travado@gmail.com>
2022-12-30 17:00:57 +0100

    First graphs - RLM & KNN. RLM Optimized

--- preprocessing_treatment.py
+++ preprocessing_treatment.py
@@ -15,7 +15,34 @@ from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 
+from sklearn.feature_selection import RFE
+
 from sklearn import metrics
+
+def plot_graph(target, y_pred, title, name, metricas):
+        # ** CÁLCULO DE LAS MÉTRICAS DE EVALUACIÓN
+        MAE = metricas['MAE'](target, y_pred)
+        RMSE = metricas['RMSE'](target, y_pred)
+        #MAPE = metricas['MAPE'](target, y_pred)
+        R2 = metricas['R2'](target, y_pred)
+        
+        # ** PLOT GRAPH
+        fig, ax = plt.subplots()
+        ax.scatter(target, y_pred, edgecolors=(0,0,0), s=1)
+        ax.plot([target.min(), target.max()],
+                [y_pred.min(), y_pred.max()], 'k--', lw=1)
+        x_min, x_max = ax.get_xlim()
+        x = [x_min, x_max]
+        ax.plot(x, x, linestyle="dashed", marker="none", lw=1)
+        ax.set_xlabel('Valor real de la clase')
+        ax.set_ylabel('Predicción')
+        ax.spines['bottom'].set_linewidth(1)
+        ax.spines['left'].set_linewidth(1)
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        title_ = "MAE: %.3f   RMSE: %.3f  R2: %.3f --- " % (MAE, RMSE, R2)
+        plt.title(title_ + str(title))
+        plt.savefig("ML - Data analysis - Olive grove plantations/graphs/" + str(name), dpi=300)
 
 # ** LECTURA DEL ARCHIVO ORIGINAL CSV
 df = pd.read_csv("ML - Data analysis - Olive grove plantations/csv/csv_result-sds_PICA_H1.csv", 
@@ -62,9 +89,9 @@ for i in range(len(scores_pred)):
         indexes.append(i)
 df_norm.drop(index=indexes, inplace=True, axis=0)
 
+
 # ** MODELADO SUPERVISADO - REGRESIÓN LINEAL MÚLTIPLE
 reg = LinearRegression()
-
 # ** DEFINICIÓN DE MÉTRICAS
 metricas = {
     'MAE' : lambda y, y_pred:
@@ -76,72 +103,45 @@ metricas = {
     'R2' : lambda y, y_pred:
             metrics.r2_score(y, y_pred)
 }
-
 df_np = df_norm.to_numpy()
 target = df_np[:, -1]
 data = df_np[:, :-1]
-
 # ** PREDICCIÓN DE LA VARIABLE OBJETIVO - CLASE (REGRESIÓN)
 seed = 1
 y_pred = model_selection.cross_val_predict(reg, data, target, 
                            cv = KFold(n_splits=10, random_state=seed, shuffle=True))
-
-# ** CÁLCULO DE LAS MÉTRICAS DE EVALUACIÓN
-MAE = metricas['MAE'](target, y_pred)
-RMSE = metricas['RMSE'](target, y_pred)
-#MAPE = metricas['MAPE'](target, y_pred)
-R2 = metricas['R2'](target, y_pred)
-
 # ** GRÁFICA MATPLOTLIB
-fig, ax = plt.subplots()
-ax.scatter(target, y_pred, edgecolors=(0,0,0), s=1)
-ax.plot([target.min(), target.max()],
-        [y_pred.min(), y_pred.max()], 'k--', lw=1)
-x_min, x_max = ax.get_xlim()
-x = [x_min, x_max]
-ax.plot(x, x, linestyle="dashed", marker="none", lw=1)
-ax.set_xlabel('Valor real de la clase')
-ax.set_ylabel('Predicción')
-ax.spines['bottom'].set_linewidth(1)
-ax.spines['left'].set_linewidth(1)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-title = "MAE: %.3f   RMSE: %.3f  R2: %.3f (RLM)" % (MAE, RMSE, R2)
-plt.title(title)
-plt.savefig("ML - Data analysis - Olive grove plantations/graphs/RLM.png", dpi=300)
+plot_graph(target, y_pred, "REG", "RLM.png", metricas)
+
+
+# ** OPTMIZACIÓN DEL ALGORITMO CON ELIMINACIÓN RECURSIVA - SELECCIÓN DE ATRIBUTOS
+reg_opt = LinearRegression()
+# Aplicamos la eliminación recursiva de atributos
+n_features = int(0.9 * len(df_norm.columns))
+rfe = RFE(estimator=reg_opt, n_features_to_select=n_features)
+fit = rfe.fit(data, target)
+# Seleccionamos los atributos más importantes
+selected_features = fit.support_
+seed = 1
+y_pred = model_selection.cross_val_predict(reg, data[:, selected_features], target, 
+                           cv = KFold(n_splits=10, random_state=seed, shuffle=True))
+# ** GRÁFICA MATPLOTLIB
+plot_graph(target, y_pred, "REG OPT", "RLM_Optimized.png", metricas)
+
 
 # ** MODELADO SUPERVISADO - KNN
 k = 10
-reg = KNeighborsRegressor(n_neighbors=k)
-
+knn = KNeighborsRegressor(n_neighbors=k)
 # ** PREDICCIÓN DE LA VARIABLE OBJETIVO - CLASE (KNN)
 seed = 1
-y_pred = model_selection.cross_val_predict(reg, data, target, 
+y_pred = model_selection.cross_val_predict(knn, data, target, 
                            cv = KFold(n_splits=10, random_state=seed, shuffle=True))
-
-# ** CÁLCULO DE LAS MÉTRICAS DE EVALUACIÓN
-MAE = metricas['MAE'](target, y_pred)
-RMSE = metricas['RMSE'](target, y_pred)
-#MAPE = metricas['MAPE'](target, y_pred)
-R2 = metricas['R2'](target, y_pred)
-
 # ** GRÁFICA MATPLOTLIB
-fig, ax = plt.subplots()
-ax.scatter(target, y_pred, edgecolors=(0,0,0), s=1)
-ax.plot([target.min(), target.max()],
-        [y_pred.min(), y_pred.max()], 'k--', lw=1)
-x_min, x_max = ax.get_xlim()
-x = [x_min, x_max]
-ax.plot(x, x, linestyle="dashed", marker="none", lw=1)
-ax.set_xlabel('Valor real de la clase')
-ax.set_ylabel('Predicción')
-ax.spines['bottom'].set_linewidth(1)
-ax.spines['left'].set_linewidth(1)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-title = "MAE: %.3f   RMSE: %.3f  R2: %.3f (KNN)" % (MAE, RMSE, R2)
-plt.title(title)
-plt.savefig("ML - Data analysis - Olive grove plantations/graphs/KNN.png", dpi=300)
+plot_graph(target, y_pred, "KNN", "KNN.png", metricas)
+
+
+
+
 
 
 
