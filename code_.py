f8068c82cc4108b64060b92318122ef4f558f827
migueltravado <miaje.travado@gmail.com>
2023-01-17 13:14:30 +0100

    CRISP-DM Done

--- code_.py
+++ code_.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import seaborn as sns
+
 
 from math import sqrt
 from scipy import stats
@@ -15,6 +18,7 @@ from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
 from sklearn.feature_selection import RFE
 
@@ -60,11 +64,10 @@ def plot_graph(target, y_pred, title, name, metricas, aux = 0):
                 title_ = "ACC: %.3f   PREC: %.3f  RECALL: %.2f  F1: %.3f --- " % (ACC, PREC, RECALL, F1)
         plt.title(title_ + str(title))
         plt.savefig("graphs/" + str(name), dpi=300)
-        plt.show()
 
 # ** LECTURA DEL ARCHIVO ORIGINAL CSV
 df_H1 = pd.read_csv("csv/csv_result-sds_PICA_H1.csv", 
-                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip') # na_values=['','?']
+                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip') 
 df_H2 = pd.read_csv("csv/csv_result-sds_PICA_H2.csv", 
                  decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip')
 df_H3 = pd.read_csv("csv/csv_result-sds_PICA_H3.csv", 
@@ -72,9 +75,21 @@ df_H3 = pd.read_csv("csv/csv_result-sds_PICA_H3.csv",
 df_H4 = pd.read_csv("csv/csv_result-sds_PICA_H4.csv", 
                  decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip')
 
+df_T1 = pd.read_csv("csv/csv_result-sds_TRAM_H1.csv", 
+                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip') 
+df_T2 = pd.read_csv("csv/csv_result-sds_TRAM_H2.csv", 
+                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip')
+df_T3 = pd.read_csv("csv/csv_result-sds_TRAM_H3.csv", 
+                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip')
+df_T4 = pd.read_csv("csv/csv_result-sds_TRAM_H4.csv", 
+                 decimal=".", delimiter=",", na_values=['','?', ' '], on_bad_lines='skip')
+
 df = pd.concat([df_H1, df_H2, df_H3, df_H4], axis=0)
+df = pd.concat([df_T1, df_T2, df_T3, df_T4], axis=0)
 
 df = df_H1
+
+
 
 # ** TRATAMIENTO VALORES NULOS. SUSTITUCIÓN IN-PLACE POR MEDIA ARITMÉTICA
 n_instances = df.shape[0]
@@ -113,6 +128,14 @@ df_norm = df_norm.round(decimals=6)
 #     if scores_pred[i] < threshold:
 #         indexes.append(i)
 # df_norm.drop(index=indexes, inplace=True, axis=0)
+
+###################
+fig, ax = plt.subplots()
+ax.set_xlabel('Valor')
+plt.boxplot(df_norm["CLASS"], vert=False)
+plt.title("CLASE")
+plt.savefig("graphs/boxplot_CLASS.png", dpi=300)
+#################
 
 # ** GUARDADO DE CSV CON LOS DATOS NORMALIZADOS Y VALORES NULOS REEMPLAZADOS
 #df_norm.to_csv('ML - Data analysis - Olive grove plantations/csv_processed/csv_result-sds_PICA_processed.csv', sep=',')
@@ -159,6 +182,18 @@ reg_opt = LinearRegression()
 n_features = int(0.75 * len(df_norm.columns))
 rfe = RFE(estimator=reg_opt, n_features_to_select=n_features)
 fit = rfe.fit(data, target)
+# Genera gráfica para evaluación de RFE
+scores = rfe.ranking_
+fig, ax = plt.subplots()
+plt.bar(range(len(scores)), scores)
+plt.xlabel('Atributos')
+plt.ylabel('Puntuación de importancia')
+ax.spines['bottom'].set_linewidth(1)
+ax.spines['left'].set_linewidth(1)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.savefig("graphs/valoración_RFE.png",dpi=300)
+
 # Seleccionamos los atributos más importantes
 selected_features = fit.support_
 seed = 1
@@ -167,6 +202,20 @@ y_pred = model_selection.cross_val_predict(reg, data[:, selected_features], targ
 # ** GRÁFICA MATPLOTLIB
 plot_graph(target, y_pred, "REG OPT", "RLM_Optimized.png", metricas_rg)
 
+
+# ** Boxplot de la distribución de los atributos normalizados
+n_features = int(0.05 * len(df_norm.columns))
+rfe = RFE(estimator=reg_opt, n_features_to_select=n_features)
+fit = rfe.fit(data, target)
+selected_features = fit.support_
+# Visualización conceptual de outliers
+# Create a figure and subplot axes for creating a grid plot
+df_norm_selected = df_norm.loc[:, np.compress(selected_features, df_norm.columns)]
+fig, axs = plt.subplots(nrows=len(df_norm_selected.columns), figsize=(10, 18.5), sharex=True)
+for i, column in enumerate(df_norm_selected.columns):
+    sns.boxplot(data=df_norm_selected, x=column, ax=axs[i])
+fig.tight_layout()
+plt.savefig("graphs/Cajas-Bigotes_Outliers.png",dpi=300)
 
 # ** MODELADO SUPERVISADO - KNN
 k = 10
@@ -179,17 +228,6 @@ y_pred = model_selection.cross_val_predict(knn, data, target,
 plot_graph(target, y_pred, "KNN k = " + str(k), "KNN.png", metricas_rg)
 
 
-# # ** MODELADO CATEGÓRICO - LOGR
-# logr = LogisticRegression(solver='sag', max_iter=100, random_state=seed, multi_class='auto')
-
-# bins = 20
-# target_cat = pd.cut(target, bins, labels=[i for i in range(bins)])
-
-# # ** PREDICCIÓN DE LA VARIABLE OBJETIVO - CLASE (LOGR)
-# y_pred = model_selection.cross_val_predict(logr, data, target_cat, cv = KFold(n_splits=10, random_state=seed, shuffle=True))
-
-# # ** GRÁFICA MATPLOTLIB
-# plot_graph(target_cat, y_pred, "LOGR" + str(k), "LOGR.png", metricas_ct, 1)
 
 
 
